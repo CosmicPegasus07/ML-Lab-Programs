ec4d0c2150fd54697ffbfa626194644eec066b13
Mohammed Afeef Hussain <89835263+CosmicPegasus07@users.noreply.github.com>
2023-01-13 08:43:07 +0530

    shorter

--- Easy Programs/linear_reg.py
+++ Easy Programs/linear_reg.py
@@ -1,26 +1,21 @@
 import numpy as np
-import matplotlib.pyplot as plt
-def estimate_coef(x,y):
-    n=np.size(x)
-    m_x,m_y=np.mean(x),np.mean(y)
-    SS_xy=np.sum(y*x)-n*m_y*m_x
-    SS_xx=np.sum(x*x)-n*m_x*m_x
-    b_1=SS_xy/SS_xx
-    b_0=m_y-b_1*m_x
-    return(b_0,b_1)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 
-def plot_regression_line(x,y,b_0,b_1):
-    plt.scatter(x,y,color='m',marker='o',s=30)
-    y_pred=b_0+b_1*x
-    plt.plot(x,y_pred,color='g')
-    plt.xlabel('x')
-    plt.xlabel('y')
-    plt.show()
-    
-def main():
-    x=np.array([0,1,2,3,4,5,6,7,8,9])
-    y=np.array([1,3,2,5,7,8,8,9,10,12])
-    b_0,b_1=estimate_coef(x,y)
-    print('Estimated coefficients:\nb_0={}\nb_1={}'.format(b_0,b_1))
-    plot_regression_line(x,y,b_0,b_1)
-main()
+# Generate some example data
+np.random.seed(0)
+x = np.random.rand(100, 1)
+y = 2 + 3 * x + np.random.rand(100, 1)
+
+# Create and fit the linear regression model
+model = LinearRegression()
+model.fit(x, y)
+
+# Make predictions on the training data
+y_pred = model.predict(x)
+
+# Calculate and print the model's performance metrics
+mse = mean_squared_error(y, y_pred)
+r2 = r2_score(y, y_pred)
+print("Mean squared error: ", mse)
+print("R-squared: ", r2)
