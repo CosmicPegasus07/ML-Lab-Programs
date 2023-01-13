9d5e9506e28021d07bd4b972a0289eadd8460b82
Mohammed Afeef Hussain <89835263+CosmicPegasus07@users.noreply.github.com>
2023-01-13 08:42:41 +0530

    shorter

--- Easy Programs/backprop.py
+++ Easy Programs/backprop.py
@@ -1,50 +1,42 @@
 import numpy as np
-X=np.array(([2,9],[1,5],[3,6]))
-y=np.array(([92],[86],[89]))
-y=y/100
-#Sigmoid function
+
+# Define the sigmoid function for activation
 def sigmoid(x):
-    return 1/(1+np.exp(-x))
-#Derivate of Sigmoid function
-def derivatives_sigmoid(x):
-    return x*(1-x)
-#Variable Initialization
-epoch=10000
-lr=0.1
-inputlayer_neurons=2
-hiddenlayer_neurons=3
-outputlayer_neurons=1
-#weight and bias initialization
-wh=np.random.uniform(size=(inputlayer_neurons,hiddenlayer_neurons))
-bias_hidden=np.random.uniform(size=(1,hiddenlayer_neurons))
-weight_hidden=np.random.uniform(size=(hiddenlayer_neurons,outputlayer_neurons))
-bias_output=np.random.uniform(size=(1,outputlayer_neurons))
-for i in range(epoch):
-    #Forward Propagation
-    hinp1=np.dot(X,wh)
-    hinp=hinp1+bias_hidden
-    hlayer_activation=sigmoid(hinp)
-    
-    outinp1=np.dot(hlayer_activation,weight_hidden)
-    outinp=outinp1+bias_output
-    output=sigmoid(outinp)
-    
-    #Backpropagation
-    EO=y-output
-    outgrad=derivatives_sigmoid(output)
-    d_output=EO*outgrad
-    
-    EH=d_output.dot(weight_hidden.T)
-    hiddengrad=derivatives_sigmoid(hlayer_activation)
-    d_hiddenlayer=EH*hiddengrad
-    
-    #update the weights
-    weight_hidden+=hlayer_activation.T.dot(d_output) * lr
-    bias_hidden+=np.sum(d_hiddenlayer,axis=0,keepdims=True) * lr
-    
-    wh+=X.T.dot(d_hiddenlayer) * lr
-    bias_output=np.sum(d_output,axis=0,keepdims=True) * lr
-    
-print("Input: \n",str(X))
-print("Actual Output: \n",str(y))
-print("Predicted Output: \n",output)
+    return 1 / (1 + np.exp(-x))
+
+# Define the derivative of the sigmoid function
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
+# Define the input and output data
+x = np.array([[0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, 1]])
+y = np.array([[0], [1], [1], [0]])
+
+# Initialize the weights randomly
+np.random.seed(1)
+weights_1 = 2 * np.random.random((3, 4)) - 1
+weights_2 = 2 * np.random.random((4, 1)) - 1
+
+# Set the number of training iterations
+num_iterations = 10000
+
+# Perform backpropagation
+for i in range(num_iterations):
+    # Forward propagation
+    layer_1 = sigmoid(np.dot(x, weights_1))
+    layer_2 = sigmoid(np.dot(layer_1, weights_2))
+
+    # Calculate the error
+    error = y - layer_2
+
+    # Backpropagation
+    layer_2_delta = error * sigmoid_derivative(layer_2)
+    layer_1_error = layer_2_delta.dot(weights_2.T)
+    layer_1_delta = layer_1_error * sigmoid_derivative(layer_1)
+
+    # Update the weights
+    weights_2 += layer_1.T.dot(layer_2_delta)
+    weights_1 += x.T.dot(layer_1_delta)
+
+# Print the final output of the network
+print(layer_2)
